a798063fa5cbc0678e30b74b2914c2b71d562b82
NaidaSma <naida.smailbegovic@stu.ibu.edu.ba>
2023-06-04 03:12:11 +0200

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'products',
+    path: 'homepage',
+    loadChildren: () =>
+      import('./modules/homepage/homepage.module').then((m) => m.HomepageModule),
+  },
+  {
+    path: 'product',
     loadChildren: () =>
       import('./modules/products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'shop',
+    loadChildren: () =>
+      import('./modules/product-page/product-page.module').then((m) => m.ProductPageModule),
+  },
+  {
+    path: 'contactUs',
+    loadChildren: () =>
+      import('./modules/contact-us/contact-us.module').then((m) => m.ContactUsModule),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./modules/homepage/homepage.module').then((m) => m.HomepageModule),
   }
 ];
 
